0bf56b5cac984c77657d24c58211e8468eab79bc
Ishan Dutta <ishandutta0098@gmail.com>
2021-07-22 11:17:19 +0530

    Swap Elements of Array

---
+++ fork-cpp/module-2 (arrays and strings)/2. need-some-change.cpp
@@ -0,0 +1,20 @@
+/*Function to swap array elements
+* arr : array input
+* sizeof_array : number of elements in array
+*/
+class Solution{
+  public:
+    void swapElements(int arr[], int sizeof_array){
+        
+        if (sizeof_array > 2) {
+        
+            int temp;
+            
+            for (int i = 0; i < sizeof_array - 2; i++) {
+                temp = arr[i];
+                arr[i] = arr[i+2];
+                arr[i+2] = temp;
+            }
+        }
+    }
+};
